f971994ac750410f3b813262ff8804587efdd1bb
Sindre Wilting <sindrewilting@gmail.com>
2019-11-14 22:11:05 +0100

    new beta

--- Source/Webpack/webpack.ts
+++ Source/Webpack/webpack.ts
@@ -2,24 +2,22 @@
 *  Copyright (c) Dolittle. All rights reserved.
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
-import { webpack as typescriptWebpack, WebpackEnvironment } from '@dolittle/typescript.webpack';
+import { webpack as typescriptWebpack } from '@dolittle/typescript.webpack';
 import { Configuration } from "webpack";
+import path from 'path';
 
 const aureliaPlugin = require("aurelia-webpack-plugin");
 
 export function webpack(dirname: string, settingsCallback?: (config: Configuration) => void) {
-    return (env: WebpackEnvironment = {}) => {
-        let config = typescriptWebpack(dirname, _ => {
-            _.entry = {
-                app: ['aurelia-bootstrapper']
-            };
-            _.plugins!.push(new aureliaPlugin.AureliaPlugin(),
-                new aureliaPlugin.ModuleDependenciesPlugin({
-                    'aurelia-testing': ['./compile-spy', './view-spy']
-                }));
-            if (typeof settingsCallback === 'function') settingsCallback(_);
-        });
-        
-        return config;
-    }
+    return typescriptWebpack(dirname, _ => {
+        _.entry = {
+            app: ['aurelia-bootstrapper']
+        };
+        _.plugins!.push(new aureliaPlugin.AureliaPlugin(),
+            new aureliaPlugin.ModuleDependenciesPlugin({
+                'aurelia-testing': ['./compile-spy', './view-spy']
+            }));
+        _.resolve!.alias!['aurelia-binding'] = path.resolve(dirname, 'node_modules/aurelia-binding');
+        if (typeof settingsCallback === 'function') settingsCallback(_);
+    });
 }
